5e7bd717ab073778001905c11569d582e56a0ec7
Aleksey Mironov <77401319+mirlex1986@users.noreply.github.com>
2021-10-01 13:38:02 +0300

    Initial Commit

---
+++ MyHomeTV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHomeTV
+//
+//  Created by Aleksey Mironov on 01.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
